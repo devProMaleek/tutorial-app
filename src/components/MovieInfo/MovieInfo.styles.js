0199e97303957d50c5b08a2c99fc519e8d3eeb4f
devProMaleek <milikiadbay@gmail.com>
2021-09-10 23:30:19 +0100

    Updated the MovieInfo component

---
+++ src/components/MovieInfo/MovieInfo.styles.js
@@ -0,0 +1,14 @@
+// Import the styled component library
+import styled from 'styled-components';
+
+// Import files from config folder
+import { IMAGE_BASE_URL, BACKDROP_SIZE } from '../../config';
+
+// Export the Wrapper styled component
+export const Wrapper = styled.div``;
+
+// Export the Content styled component.
+export const Content = styled.div``;
+
+// Export the Text styled component.
+export const Text = styled.div``;
