7f24cc7ee502ac1a752fb64d17fe9eabf2fcebb4
devProMaleek <milikiadbay@gmail.com>
2021-09-10 11:29:22 +0100

    Styled the BreadCrumb component

--- src/components/BreadCrumb/BreadCrumb.styles.js
+++ src/components/BreadCrumb/BreadCrumb.styles.js
@@ -2,7 +2,30 @@
 import styled from 'styled-components';
 
 // Export the wrapper styled component
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height:  70px;
+  background: var(--mediumGrey);
+  color: var(--white);
+`;
 
 // Export the Content styled component
-export const Content = styled.div``;
+export const Content = styled.div`
+  display: flex;
+  width: 100%;
+  max-width: var(--maxWidth);
+  padding: 0 20px;
+  
+  span {
+    font-size: var(--fontMedium);
+    color: var(--white);
+    padding-right: 10px;
+    
+    @media screen and (max-width: 768px) {
+      font-size: var(--fontSmall);
+    }
+  }
+`;
