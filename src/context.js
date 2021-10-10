9afe1e3853b9d275bcef1267d343951943c7e725
devProMaleek <milikiadbay@gmail.com>
2021-10-10 23:57:14 +0100

    Created a global context

---
+++ src/context.js
@@ -0,0 +1,17 @@
+// Import the necessary libraries
+import React, { useState, createContext } from 'react';
+
+// Create and Export createContext.
+export const Context = createContext();
+
+// Provider
+
+const UserProvider = ({ children }) => {
+    const [state, setState] = useState(undefined);
+
+    return (
+        <Context.Provider value={[state, setState]}>{children}</Context.Provider>
+    )
+}
+
+export default UserProvider;
